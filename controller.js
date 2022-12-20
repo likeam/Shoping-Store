bb46bb64ff2f8855c5c5f45ec213bf128e48b7eb
AbdulRehman <likeam99@gmail.com>
2022-12-20 10:32:35 +0500

    Weather api rendering

--- controller.js
+++ controller.js
@@ -8,7 +8,7 @@ let listItem;
 function addItemsToCart(){
     const cart= JSON.parse(localStorage.getItem('cart')  )
     const value =cart != undefined ?cart.length:  0 
-    console.log(' cart', cart, value, cartBox)
+    
     cartBox.innerHTML = value
 
 }
@@ -35,27 +35,6 @@ alert('item added already')
 }
 addItemsToCart()
 
-console.log(cart)
-    // for( let key in localSto
-    //      ) {
-    //     if (!localStorage.hasOwnProperty(key)) {
-    //         localStorage.setItem("cart", id);
-    //     }
-    //     console.log(`${key}: ${localStorage.getItem(key)}`);
-    // }
-
-    // localStorage.key.map(e => {
-
-    //     if(e === id){
-    //         console.log(e, id);
-    //         return
-    //     }else{
-    //         localStorage.setItem("cart", id);
-    //         console.log(e, id);
-    //     }
-    // })
-    
-    
 }
 
 
@@ -65,8 +44,6 @@ console.log(cart)
             const response = await fetch('https://dummyjson.com/products');
             const data = await response.json();
 
-            
-            console.log(data.products);
 
             let card = data.products.map((item, i) => {
 
@@ -83,27 +60,26 @@ console.log(cart)
             };
 
             const displayCard = `
-            <div class = "card">
-            <div class="card-header">
-            <img src="${listItem.image}" alt="${listItem.image}" />
-            </div>
-            <div class="listItem-body">
-                <span class="tag tag-teal">${listItem.brand}</span>
-                <h4>${listItem.title}</h4>
-                <p>${listItem.description}</p>
-                <div class="user">
-                    <img src="${listItem.thumbnail}" alt="${listItem.thumbnail}" />
-                    <div class="user-info">
-                        <h5>${listItem.price}</h5>
-                        <small>${listItem.id}</small>
-                        
+                <div class = "card">
+                    <div class="card-header">
+                        <img src="${listItem.image}" alt="${listItem.image}" />
                     </div>
-                </div>
-            </div>
+                    <div class="listItem-body">
+                        <span class="tag tag-teal">${listItem.brand}</span>
+                        <h4>${listItem.title}</h4>
+                        <p>${listItem.description}</p>
+                        <div class="user">
+                            <img src="${listItem.thumbnail}" alt="${listItem.thumbnail}" />
+                            <div class="user-info">
+                                <h5>${listItem.price}</h5>
+                                <small>${listItem.id}</small>                        
+                            </div>
+                        </div>
+                    </div>
 
-            <button data-id = ${listItem.id} onclick="myFunction(event)" class="button" >Add To Cart</button>
-            </div>
-            </div>
+                    <button data-id = ${listItem.id} onclick="myFunction(event)" class="button" >Add To Cart</button>
+                </div>
+            
             `;
             constainer.insertAdjacentHTML("afterbegin", displayCard);
         });
@@ -116,10 +92,6 @@ console.log(cart)
 
     showCards();
 
-    // localStorage.setItem("lastname", "Smith");
-// localStorage.getItem("lastname");
-
-// localStorage.clear();
 
 //https://www.weatherapi.com/
 // 893b7179fa314d0b90b155024221812
@@ -139,19 +111,11 @@ const fetchResults = async () =>{
 
     console.log(data2.location.name);
 
-    const weatherDisplay = `
-    <div class="carousel-item active">
-    <div class="d-flex justify-content-between mb-4 pb-2">
-        <div>
-        <h2 class="display-2"><strong>${data2.current.temp_c}°C</strong></h2>
-        <p class="text-muted mb-0">${data2.location.name}</p>
+    const weatherDisplay = `    
+        <div class="display-1">
+            <h2 class="display-2"><strong>${data2.current.temp_c}°C</strong></h2>
+            <h2 class="display-3"><strong>${data2.location.name}</strong></h2>
         </div>
-        <div>
-        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-weather/ilu3.webp"
-            width="150px">
-        </div>
-    </div>
-    </div>
     `;
 
     inner.innerHTML = weatherDisplay;
